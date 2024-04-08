73787fbdd63ae6082250fe2dd8a127c2526be3de
Rohana <rthissqa91@gmail.com>
2024-04-08 15:18:51 +0530

    add-initicommit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from './Component/Main';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        test react app
+        <h1>
+          test header
+        </h1>
+      </div>
+      <Main />
+    </>
+
+  ); 
+}
+
+export default App;
---
+++ src/Component/Main.js
@@ -0,0 +1,21 @@
+
+
+
+
+const name =  'Rohana';
+const position =  'developer';
+
+function Main(props){
+    return(
+        <div>
+            This is main component.
+
+            <ui>
+                <li>namr === {name}</li>
+                <li>position === {position}</li>
+            </ui>
+        </div>
+    )
+}
+
+export default Main;
